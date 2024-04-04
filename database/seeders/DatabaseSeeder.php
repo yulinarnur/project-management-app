78a408f1ea57046aebcaf802616aa5510a735afe
YulinarNR <yulinarnurrahma@gmail.com>
2024-04-05 00:42:13 +0700

    database-model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Yulinar',
+            'email' => 'yulinar@example.com',
+            'password' => bcrypt('456.456A'),
+            'email_verified_at' => time()
         ]);
     }
 }
